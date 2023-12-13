250fc03decae112e52e69f2bfbf8bf43e66a3196
yasmine-khaldi <khaldiyasmine27@gmail.com>
2023-12-13 16:00:37 +0100

    yasmine commit

---
+++ MovieStore/Models/Domain/MovieGenre.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieStore.Models.Domain
+{
+    public class MovieGenre
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int GenreId { get; set; }
+    }
+}
